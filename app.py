2802df4e2e32a6f62c60cc98f0ca7a1064d40eb9
GAUTAM NAHAR <nahargautam001@gmail.com>
2023-01-25 14:00:29 +0530

    connected to local database

--- app.py
+++ app.py
@@ -1,24 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session ,url_for, redirect
 import pickle
 import numpy as np
+import pymysql
 
 app = Flask(__name__)
+app.secret_key = "qwerty123"
+
+con = pymysql.connect(host='localhost', port = 3306, user='root', password='Mysql@123', database="whowins")
+cur = con.cursor()
 
 @app.route('/')
-def home():
+def index():
 	return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('prediction.html', username = session['username'])
 
 @app.route('/SigninAuthentication', methods=['POST','GET'])
 def SigninAuthentication():
+    msg = ""
     if request.method == 'POST':
         username=request.form['username']
         password=request.form['password']
-        
-        if (username=='har123' and password=='9653'):
-            
-            return render_template('prediction.html')
+
+        if username == "" or password == "":
+            msg = 'All Fields are required!'
         else:
-            return render_template('again.html')
+            cur.execute('select * from accounts where username=%s and password=%s',(username,password))
+            row = cur.fetchone()
+            if row == None:
+                msg = 'Invalid Credentials. Please Try Again!'
+            else:
+                session['loggedin'] = True
+                session['username'] = row[1]
+                return redirect(url_for('home'))
+    return render_template('index.html',msg)
 
 
 if __name__ == '__main__':
