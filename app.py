855a7ba009e8fd1e1520e5c13f2e82f91ac5e796
GAUTAM NAHAR <nahargautam001@gmail.com>
2023-01-27 13:09:32 +0530

    registration page added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session ,url_for, redirect
+from flask import Flask, render_template, request, session, url_for, redirect
 import pickle
 import numpy as np
 import pymysql
@@ -6,37 +6,61 @@ import pymysql
 app = Flask(__name__)
 app.secret_key = "qwerty123"
 
-con = pymysql.connect(host='localhost', port = 3306, user='root', password='Mysql@123', database="whowins")
+con = pymysql.connect(host='localhost', port=3306, user='root', password='Mysql@123', database="whowins")
 cur = con.cursor()
+
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/home')
 def home():
-    return render_template('prediction.html', username = session['username'])
+    return render_template('prediction.html', username=session['username'])
 
-@app.route('/SigninAuthentication', methods=['POST','GET'])
+
+@app.route('/SigninAuthentication', methods=['POST', 'GET'])
 def SigninAuthentication():
     msg = ""
     if request.method == 'POST':
-        username=request.form['username']
-        password=request.form['password']
+        username = request.form['username']
+        password = request.form['password']
 
         if username == "" or password == "":
             msg = 'All Fields are required!'
         else:
-            cur.execute('select * from accounts where username=%s and password=%s',(username,password))
+            cur.execute('select * from accounts where username=%s and password=%s', (username, password))
             row = cur.fetchone()
-            if row == None:
+            if row is None:
                 msg = 'Invalid Credentials. Please Try Again!'
             else:
                 session['loggedin'] = True
                 session['username'] = row[1]
                 return redirect(url_for('home'))
-    return render_template('index.html',msg)
+    return render_template('index.html', msg)
+
+
+@app.route('/Registration')
+def Register():
+    return render_template('registration.html')
+
+
+@app.route('/RegisterAuthentication', methods=['POST', 'GET'])
+def RegisterAuthentication():
+    msg = ""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        if username == "" or password == "" or email == "":
+            msg = 'All Fields are required!'
+        else:
+            cur.execute("INSERT INTO accounts (username, password, email) VALUES (%s,%s,%s)", (username, password, email))
+            return redirect(url_for('home'))
+    return render_template('index.html', msg)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
