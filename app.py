407ecdcbe5266085528f495d7bd15d7ceab95b96
GAUTAM NAHAR <nahargautam001@gmail.com>
2023-01-30 20:19:42 +0530

    otp verification and validation done

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request, session, url_for, redirect
+import random
+
+from flask import Flask, render_template, request, session, redirect, url_for
 import pickle
 import numpy as np
+import os
 import pymysql
+from twilio.rest import Client
 
 app = Flask(__name__)
-app.secret_key = "qwerty123"
+app.secret_key = os.urandom(24)
 
 con = pymysql.connect(host='localhost', port=3306, user='root', password='Mysql@123', database="whowins")
 cur = con.cursor()
@@ -12,12 +16,18 @@ cur = con.cursor()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'userid' in session:
+        return redirect('/home')
+    else:
+        return render_template('index.html')
 
 
 @app.route('/home')
 def home():
-    return render_template('prediction.html', username=session['username'])
+    if 'userid' in session:
+        return render_template('home.html', username=session['username'])
+    else:
+        return redirect('/')
 
 
 @app.route('/SigninAuthentication', methods=['POST', 'GET'])
@@ -35,9 +45,10 @@ def SigninAuthentication():
             if row is None:
                 msg = 'Invalid Credentials. Please Try Again!'
             else:
-                session['loggedin'] = True
                 session['username'] = row[1]
-                return redirect(url_for('home'))
+                session['userid'] = row[0]
+                session['mobno'] = row[4]
+                return redirect('/home')
     return render_template('index.html', msg)
 
 
@@ -53,14 +64,72 @@ def RegisterAuthentication():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
+        mobno = request.form['mobno']
 
         if username == "" or password == "" or email == "":
             msg = 'All Fields are required!'
         else:
-            cur.execute("INSERT INTO accounts (username, password, email) VALUES (%s,%s,%s)", (username, password, email))
+            cur.execute("INSERT INTO accounts (username, password, email, mobileno) VALUES (%s,%s,%s,%s)",
+                        (username, password, email, mobno))
             con.commit()
-            return redirect(url_for('home'))
+            cur.execute('select * from accounts where username=%s and password=%s', (username, password))
+            row = cur.fetchone()
+            session['username'] = username
+            session['userid'] = row[0]
+            session['mobno'] = mobno
+            return redirect('/home')
     return render_template('index.html', msg)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('userid')
+    session.pop('username')
+    session['loggedin'] = False
+    session.pop('mobno')
+    return redirect('/')
+
+
+@app.route('/OtpAuthentication')
+def OtpAuthentication():
+    if 'userid' in session:
+        mobno = session['mobno']
+        val = getOTPApi(mobno)
+        if val:
+            return render_template('getotp.html')
+    else:
+        return redirect('/')
+
+
+def getOTPApi(mobno):
+    account_sid = 'AC176c49b0ef6b5b8a84fd359c6c3464b1'
+    auth_token = '0f1df18a595e3735dadef290473753c3'
+    client = Client(account_sid, auth_token)
+    otp = random.randrange(100000, 999999)
+    session['response'] = str(otp)
+    body = 'Your Otp is ' + str(otp)
+    message = client.messages.create(
+        messaging_service_sid='MGcc756f34d7fc6c331b6eced9137df867',
+        body=body,
+        to=mobno
+    )
+    if message.sid:
+        return True
+    else:
+        return False
+
+
+@app.route('/ValidateOTP', methods=['Post'])
+def ValidateOTP():
+    otpe = request.form['otpe']
+    if 'response' in session:
+        otp = session['response']
+        session.pop('response', None)
+        if otp == otpe:
+            session['loggedin'] = True
+            return redirect('/home')
+        else:
+            return redirect('/logout')
 
 
 if __name__ == '__main__':
